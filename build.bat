527d1809a19946d420301f31932e96c01442e3ea
Robert Mills <robert.mills@citrix.com>
2014-03-07 08:34:59 -0500

    Update ApiToClient Modify build script to restore from NuGet and build ApiToClient.sln.

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
+ApiToClient\NuGet\NuGet.exe restore ApiToClient\ApiToClient.sln
+
+msbuild ApiToClient\ApiToClient.sln /t:ReBuild /p:"PLatform=x86" /p:Configuration=Debug
+
 set languageDirectory=%CD%
 for %%* in (.) do set CurrDirName=%%~n*
 

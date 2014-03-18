073ad1711b4caaf6503bd327c94b3ac2b1e5a2ae
unknown <RobertMil@WRDUROBERTMILLS.citrite.net>
2014-03-18 07:38:03 -0400

    Add callbacks for ChangeDomain and Exceptions

---
+++ Core/Events/ChangeDomainCallback.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.Http;
+using ShareFile.Api.Client.Models;
+
+namespace ShareFile.Api.Client.Events
+{
+    public delegate EventHandlerResponse ChangeDomainCallback(HttpRequestMessage requestMessage, Redirection redirect);
+}
---
+++ Core/GeneratedModels/Redirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ShareFile.Api.Models;
+
+namespace ShareFile.Api.Client.Models
+{
+    public class Redirection : ODataObject
+    {
+        public string Method { get; set; }
+
+        public Zone Zone { get; set; }
+
+        public Uri Uri { get; set; }
+
+        public string Body { get; set; }
+    }
+}

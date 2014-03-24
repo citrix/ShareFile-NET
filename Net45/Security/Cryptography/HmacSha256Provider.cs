85e15b6050ce3465b6b3bc37d1dafa7e234e25a2
Robert Mills <robert.mills@citrix.com>
2014-03-24 09:12:56 -0400

    NuGet references should be sln level
    Add Net45 project that provides HmacSha256 platform implementation.

---
+++ Net45/Security/Cryptography/HmacSha256Provider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ShareFile.Api.Client.Security.Cryptography
+{
+    public class HmacSha256Provider : IHmacSha256Provider
+    {
+        static HmacSha256Provider()
+        {
+            HmacSha256ProviderFactory.Register(bytes =>
+            {
+                return new HmacSha256Provider
+                {
+                    Key = bytes
+                };
+            });
+        }
+
+        public byte[] Key { get; set; }
+        public byte[] ComputeHash(byte[] buffer)
+        {
+            var hmac = new HMACSHA256(Key);
+
+            return hmac.ComputeHash(buffer);
+        }
+    }
+}

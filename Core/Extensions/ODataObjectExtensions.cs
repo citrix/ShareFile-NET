b36587692cb802b225a606a97c92b1534e4bf5d4
Robert Mills <robert.mills@citrix.com>
2014-03-26 13:24:34 -0400

    Store Metadata in Properties field.

--- Core/Extensions/ODataObjectExtensions.cs
+++ Core/Extensions/ODataObjectExtensions.cs
@@ -7,15 +7,63 @@ namespace ShareFile.Api.Client.Extensions
     {
         public static void SetMetadata(this ODataObject oDataObject, Uri baseUri, string entitySet, string typeCast = null, ODataObjectType? type = null)
         {
-            //oDataObject.MetadataBaseUri = baseUri;
-            //oDataObject.EntitySet = entitySet;
-            //oDataObject.TypeCast = typeCast;
-            //oDataObject.Type = type.GetValueOrDefault(ODataObjectType.Entity);
+            oDataObject.Properties.Add("MetadataBaseUri", baseUri.ToString());
+            oDataObject.Properties.Add("EntitySet", entitySet);
+            oDataObject.Properties.Add("TypeCast", typeCast);
+            oDataObject.Properties.Add("Type", type.GetValueOrDefault(ODataObjectType.Entity).ToString());
         }
 
         public static Uri GetObjectUri(this ODataObject oDataObject, bool useStreamId = false)
         {
             return oDataObject.url;
+        }
+
+        public static Uri GetMetadataBaseUri(this ODataObject oDataObject)
+        {
+            string metadata;
+
+            if (oDataObject.Properties.TryGetValue("MetadataBaseUri", out metadata))
+            {
+                return new Uri(metadata, UriKind.RelativeOrAbsolute);
+            }
+            return null;
+        }
+
+        public static string GetEntitySet(this ODataObject oDataObject)
+        {
+            string entitySet;
+
+            if (oDataObject.Properties.TryGetValue("EntitySet", out entitySet))
+            {
+                return entitySet;
+            }
+            return null;
+        }
+
+        public static string GetTypeCast(this ODataObject oDataObject)
+        {
+            string typeCast;
+
+            if (oDataObject.Properties.TryGetValue("TypeCast", out typeCast))
+            {
+                return typeCast;
+            }
+            return null;
+        }
+
+        public static ODataObjectType GetODataObjectType(this ODataObject oDataObject)
+        {
+            string type;
+
+            if (oDataObject.Properties.TryGetValue("TypeCast", out type))
+            {
+                ODataObjectType @enum;
+                if (Enum.TryParse(type, out @enum))
+                {
+                    return @enum;
+                }
+            }
+            return ODataObjectType.Entity;
         }
     }
 }

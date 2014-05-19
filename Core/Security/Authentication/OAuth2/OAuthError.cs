a1370c09adccaa6ae11221ca882cdd91e0163ed1
Nick Nicholls <nick.nicholls@citrix.com>
2014-05-19 08:43:25 -0400

    Added message override to OAuthErrorException that returns the inner error message.

--- Core/Security/Authentication/OAuth2/OAuthError.cs
+++ Core/Security/Authentication/OAuth2/OAuthError.cs
@@ -32,5 +32,15 @@ namespace ShareFile.Api.Client.Security.Authentication.OAuth2
     public class OAuthErrorException : Exception
     {
         public OAuthError Error { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                if (Error == null)
+                    return base.Message;
+                return string.Format("Error: {0} | Description: {1}", Error.Error, Error.ErrorDescription);
+            }
+        }
     }
 }

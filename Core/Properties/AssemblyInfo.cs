767ab7d0eef6e9fe707cbe094e2faa1efa0a9e9d
Robert Mills <robert.mills@citrix.com>
2014-08-14 12:35:58 -0400

    Increment the version

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyProductAttribute("ShareFile.Api.Client")]
 [assembly: AssemblyTitleAttribute("ShareFile Client SDK")]
 [assembly: AssemblyVersionAttribute("3.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.0.3")]
+[assembly: AssemblyFileVersionAttribute("3.0.4")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Citrix ShareFile 2014")]
 namespace System {
     internal static class AssemblyVersionInformation {
--- build.fsx
+++ build.fsx
@@ -11,10 +11,10 @@ let authors = ["Citrix ShareFile"]
 let buildDir = "./build/"
 let packagingRoot = "./packaging/"
 let packagingDir = packagingRoot @@ "sharefile"
-let nugetVersion = getBuildParamOrDefault "nugetVersion" "3.0.3"
+let nugetVersion = getBuildParamOrDefault "nugetVersion" "3.0.4-beta01"
 // DO NOT INCREMENT THIS VALUE -- Will cause issues with PowerShell and StrongNamed versions of the assembly
 let assemblyVersion = getBuildParamOrDefault "assemblyVersion" "3.0.0"
-let assemblyFileVersion = getBuildParamOrDefault "assemblyFileVersion" "3.0.3"
+let assemblyFileVersion = getBuildParamOrDefault "assemblyFileVersion" "3.0.4"
 let nugetAccessKey = getBuildParamOrDefault "nugetkey" ""
 let nugetDestination = getBuildParamOrDefault "nugetserver" ""
 let title = "ShareFile Client SDK"

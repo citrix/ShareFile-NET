2e2e8931879a9c9fe4f9a896203828e8abfc20e3
Robert Mills <robert.mills@citrix.com>
2014-09-12 09:10:05 -0400

    - Update NuGet reference - Delete bin/obj from desintation

--- CloneForGitHub.ps1
+++ CloneForGitHub.ps1
@@ -2,4 +2,4 @@
 
 Copy-Item .\* $Destination -Recurse -Force
 
-Remove-Item $Destination -include *internal*,*.bat,*.ps*,*.fsx,.git,packages,*.nuspec -Exclude generate_coverage.bat -Recurse -Force
+Remove-Item $Destination -include *internal*,*.bat,*.ps*,*.fsx,.git,packages,*.nuspec,bin,obj -Exclude generate_coverage.bat -Recurse -Force

a1e46e6644cf32e707b725eaade1a156701fdecc
Robert Mills <robert.mills@citrix.com>
2014-09-23 09:23:04 -0400

    Update internal build scripts.

--- build.fsx
+++ build.fsx
@@ -45,7 +45,7 @@ Target "AssemblyInfo" (fun () ->
             Attribute.Version assemblyVersion
             Attribute.FileVersion assemblyFileVersion
             Attribute.Copyright "Copyright © Citrix ShareFile 2014" ]
-    let assemblyInfoInternal = [ Attribute.InternalsVisibleTo "ShareFile.Api.Client.Core.Internal" ]
+    let assemblyInfoInternal = [ Attribute.InternalsVisibleTo "ShareFile.Api.Client.Core.Internal, PublicKey=0024000004800000940000000602000000240000525341310004000001000100072814e41fdbe3ae14cca4cad075417f8154bacb64a4a8a830b5f9e453e7f119a8b9e0e7a47a9a4847f0ad88bf61056e03f17bd5c6e8a59eb9ccc20f2c21afae54a60d0a83f16b39d6907c3f7739a1166bc259a6852ffd5831dc8503a2b5c808096c8059e0092ae8b3cf4c0ca4e9ebb98385949ee83cc3fc10900c05db2210c8" ]
 
     let applyAssemblyInfo assemblyInfoFile = CreateCSharpAssemblyInfo assemblyInfoFile (if buildType = "internal" then assemblyInfo @ assemblyInfoInternal else assemblyInfo)
     [ "./Core/Properties/AssemblyInfo.cs"; "./Net45/Properties/AssemblyInfo.cs" ] |> Seq.iter applyAssemblyInfo

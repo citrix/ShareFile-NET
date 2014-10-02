63e7c18ddd6f81c68bc97773ca7439ec0cd63953
Robert Mills <robert.mills@citrix.com>
2014-10-02 15:23:59 -0400

    Update NuGet server and version

--- buildInternal.bat
+++ buildInternal.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
 "Tools\NuGet\nuget.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-"packages\FAKE\tools\Fake.exe" build.fsx "sign=false" "nugetkey=nUg3tMyP@cKag3" "nugetserver=http://10.1.245.40:81/nuget/ShareFile"
-"packages\FAKE\tools\Fake.exe" build.fsx "sign=true" "nugetkey=nUg3tMyP@cKag3" "nugetserver=http://10.1.245.40:81/nuget/ShareFile"
+"packages\FAKE\tools\Fake.exe" build.fsx "sign=false" "nugetkey=nUg3tMyP@cKag3" "nugetserver=http://sf-nuget.citrite.net:81/nuget/ShareFile"
+"packages\FAKE\tools\Fake.exe" build.fsx "sign=true" "nugetkey=nUg3tMyP@cKag3" "nugetserver=http://sf-nuget.citrite.net:81/nuget/ShareFile"
 pause

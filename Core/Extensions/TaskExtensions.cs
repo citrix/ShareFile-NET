c9053e9c36705b3ba6f89521e663830bb1239313
Robert Mills <robert.mills@citrix.com>
2014-10-14 21:54:09 -0400

    Added more extensions

--- Core/Extensions/TaskExtensions.cs
+++ Core/Extensions/TaskExtensions.cs
@@ -12,15 +12,24 @@ namespace ShareFile.Api.Client.Extensions.Tasks
         {
             try
             {
-                if (task.Status == TaskStatus.Created)
-                    task.Start();
-
                 task.Wait();
                 return task.Result;
             }
             catch (AggregateException agg)
             {
                 throw agg.Flatten().InnerExceptions.First();
+            }
+        }
+
+        public static System.Net.Http.HttpResponseMessage WaitForTask(this Task<System.Net.Http.HttpResponseMessage> task)
+        {
+            try
+            {
+                return WaitForTask<System.Net.Http.HttpResponseMessage>(task);
+            }
+            catch(TaskCanceledException cancel)
+            {
+                throw new TimeoutException("HttpRequest timed out", cancel);
             }
         }
     }

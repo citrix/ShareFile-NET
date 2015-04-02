4dd0ce847263f8ba58e31286ed4664ccecaf9263
Jenkins <sf-build@citrix.com>
2015-04-02 09:54:42 -0400

    Updating generated files: a445fb76fc07459afbc2307c82b8ee82ee0c0d36

--- Core/Models/AccountPreferences.cs
+++ Core/Models/AccountPreferences.cs
@@ -233,6 +233,8 @@ namespace ShareFile.Api.Models
 
 		public bool? EnableHomeFolders { get; set; }
 
+		public bool? EnableCustomBrandingUsesS3 { get; set; }
+
 		public override void Copy(ODataObject source, JsonSerializer serializer)
 		{
 			if(source == null || serializer == null) return;
@@ -348,6 +350,7 @@ namespace ShareFile.Api.Models
 				EnableExternalEmailArchiving = typedSource.EnableExternalEmailArchiving;
 				CanStoreItemsInShareFile = typedSource.CanStoreItemsInShareFile;
 				EnableHomeFolders = typedSource.EnableHomeFolders;
+				EnableCustomBrandingUsesS3 = typedSource.EnableCustomBrandingUsesS3;
 			}
 			else
 			{
@@ -779,6 +782,10 @@ namespace ShareFile.Api.Models
 				if(source.TryGetProperty("EnableHomeFolders", out token) && token.Type != JTokenType.Null)
 				{
 					EnableHomeFolders = (bool?)serializer.Deserialize(token.CreateReader(), typeof(bool?));
+				}
+				if(source.TryGetProperty("EnableCustomBrandingUsesS3", out token) && token.Type != JTokenType.Null)
+				{
+					EnableCustomBrandingUsesS3 = (bool?)serializer.Deserialize(token.CreateReader(), typeof(bool?));
 				}
 			}
 		}

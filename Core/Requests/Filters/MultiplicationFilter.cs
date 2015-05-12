661e6707c1da40d8190aca9d952ced48e02cb9bb
asimiqbal11 <asim.iqbal@ebricks-inc.com>
2015-05-12 20:27:46 +0500

    Added missing request filters

---
+++ Core/Requests/Filters/MultiplicationFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace ShareFile.Api.Client.Requests.Filters
+{
+    public class MultiplicationFilter : IFilter
+    {
+        public string PropertyName { get; set; }
+        public string PropertyValue1 { get; set; }
+        public string PropertyValue2 { get; set; }
+        public string LogicalOperator { get; set; }
+
+        public MultiplicationFilter(string propertyName, string value1, string logicalOperator, string value2)
+        {
+            PropertyName = propertyName;
+            PropertyValue1 = value1;
+            PropertyValue2 = value2;
+            LogicalOperator = logicalOperator;
+        }
+
+        public override string ToString()
+        {
+                return string.Format("{0} mul '{1}' {2} '{3}'", PropertyName, PropertyValue1, LogicalOperator, PropertyValue2);
+        }
+    }
+}

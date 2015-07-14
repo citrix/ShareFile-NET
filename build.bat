61b53282899375079155da6d08ceb3b7106504fe
asimiqbal11 <asim.iqbal@ebricks-inc.com>
2015-07-14 13:04:19 +0500

    Cleaned the repository

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cls
-"Tools\NuGet\nuget.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-"packages\FAKE\tools\Fake.exe" build.fsx "sign=false"
-"packages\FAKE\tools\Fake.exe" build.fsx "sign=true"
-pause
